e29c9b96b2d9c048b946357f85facc458556f883
Vitalii <appleidreserved@gmail.com>
2022-12-11 23:00:04 +0200

    fix jpg css js

--- src/block.js
+++ src/block.js
@@ -1,16 +1,17 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
-const anchorElements = document.querySelector(".thumbnails-anchor");
+const anchorElements = document.querySelectorAll(".thumbnails-anchor")
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
-    detailsTitle.innerHTML = anchor.getAttribute("data-details-image");
+    detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
 
 }
 
-for (let i=0; i<anchorElements.length; i++) {
+for (let i=0; i < anchorElements.length; i++) {
     anchorElements[i].addEventListener("click", function() {
         setDetails(anchorElements[i]);
+        
     } )
 
 }

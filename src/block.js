6daabb3098ec91207a4e803b8ec732748bf9f94b
Vitalii <appleidreserved@gmail.com>
2022-12-16 23:54:29 +0200

    р2

--- src/block.js
+++ src/block.js
@@ -3,11 +3,17 @@ const detailsTitle = document.querySelector(".details-title");
 const anchorElements = document.querySelectorAll(".thumbnails-anchor");
 const mainElement = document.querySelector(".main-class");
 const hideButtonElement = document.getElementById("hide-button");
+const audio = document.getElementById("audio");
 const HIDDEN = "hidden";
+const IS_POINT = "is-point";
 
 
 function showDetails() {
     mainElement.classList.remove(HIDDEN);
+    detailsContainer.classList.add(IS_POINT);
+    setTimeout(function(){
+        detailsContainer.classList.remove(IS_POINT);
+    })
 }
 function hideDetails() {
     mainElement.classList.add(HIDDEN);
@@ -16,7 +22,14 @@ function hideDetails() {
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
+    showDetails();
     detailsTitle.innerHTML = anchor.getAttribute("data-details-title");
+    audio.setAttribute("src", anchor.getAttribute('sound'));
+    audio.play();
+    setTimeout(function(){
+        audio.onpause();
+        audio.currendTime = 0;
+    }, 5000)
 
 }
 

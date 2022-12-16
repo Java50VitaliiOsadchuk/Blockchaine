24f05acd1b6b5991db726a72a1f5f72c0f1da543
Vitalii <appleidreserved@gmail.com>
2022-12-16 20:52:16 +0200

    hide

--- src/block.js
+++ src/block.js
@@ -1,6 +1,18 @@
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
-const anchorElements = document.querySelectorAll(".thumbnails-anchor")
+const anchorElements = document.querySelectorAll(".thumbnails-anchor");
+const mainElement = document.querySelector(".main-class");
+const hideButtonElement = document.getElementById("hide-button");
+const HIDDEN = "hidden";
+
+
+function showDetails() {
+    mainElement.classList.remove(HIDDEN);
+}
+function hideDetails() {
+    mainElement.classList.add(HIDDEN);
+}
+
 function setDetails(anchor) {
     const dataImage = anchor.getAttribute("data-details-image");
     detailsImage.src = dataImage;
@@ -15,3 +27,4 @@ for (let i=0; i < anchorElements.length; i++) {
     } )
 
 }
+hideButtonElement.addEventListener("click",hideDetails);
